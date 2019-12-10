649815aeb81a03aeb4c5b0db8f3e00c06c4b8567
Grzegorz Przybyła <grzegorz.przybyla@icloud.com>
2019-12-10 21:18:28 +0100

    Initial Commit

---
+++ PageViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PageViewer
+//
+//  Created by Grzegorz Przybyła on 10/12/2019.
+//  Copyright © 2019 Grzegorz Przybyła. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PageViewerTests/PageViewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  PageViewerTests.swift
+//  PageViewerTests
+//
+//  Created by Grzegorz Przybyła on 10/12/2019.
+//  Copyright © 2019 Grzegorz Przybyła. All rights reserved.
+//
+
+import XCTest
+@testable import PageViewer
+
+class PageViewerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
