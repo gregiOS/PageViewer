4d14362b669aa3eee6d25f8976c45e50b76a150d
Grzegorz Przybyła <grzegorz.przybyla@icloud.com>
2019-12-14 11:03:22 +0100

    Inital commit

--- PageViewer/ContentView.swift
+++ PageViewer/ContentView.swift
@@ -8,14 +8,65 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State var index: Int = 0
+    @State var offset: CGFloat = 0
+    @State var pages: [PageViewData] = [
+        PageViewData(imageNamed: "image1"),
+        PageViewData(imageNamed: "image1"),
+        PageViewData(imageNamed: "image1")
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { geometry in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .center, spacing: 0) {
+                    ForEach(self.pages) { viewData in
+                        PageView(viewData: viewData)
+                            .frame(width: geometry.size.width,
+                                   height: geometry.size.height)
+                    }
+                }
+            }
+            .content
+            .offset(x: self.offset)
+            .frame(width: geometry.size.width, alignment: .leading)
+            .gesture(
+                DragGesture()
+                    .onChanged({ value in
+                        self.offset = value.translation.width + -geometry.size.width * CGFloat(self.index)
+                    })
+                    .onEnded({ value in
+                        var offset = CGFloat(self.index) * -geometry.size.width
+                        if value.predictedEndTranslation.width < geometry.size.width / 2, self.index < self.pages.count - 1 {
+                            self.index += 1
+                            offset = CGFloat(self.index) * -geometry.size.width
+                        }
+                        if value.predictedEndTranslation.width > geometry.size.width / 2, self.index > 0 {
+                            self.index -= 1
+                                offset = CGFloat(self.index) * -geometry.size.width
+                        }
+                        withAnimation {
+                            self.offset = offset
+                        }
+                    })
+            )
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct PageViewData: Identifiable {
+    let id: String = UUID().uuidString
+    let imageNamed: String
+}
+
+struct PageView: View {
+    let viewData: PageViewData
+    var body: some View {
+        Image(viewData.imageNamed)
+            .resizable()
+            .clipped()
     }
 }
